119d7ed7f246b8a5b03e2e57e432d6b641f76b4a
Karolina <kp.zywicka@gmail.com>
2022-09-13 19:33:09 +0200

    Add new files

---
+++ map1.py
@@ -0,0 +1,38 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "darkgreen"
+    elif 1000<=elevation <3000:
+        return "purple"
+    else:
+        return "red"
+
+
+map = folium.Map(location=[38.58, -99.89], zoom_start=6, tiles="Stamen Terrain")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el, nm in zip(lat, lon, elev, name):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],radius= 9, popup= str(nm) + " " + str(el) + " m", fill_color = color_producer(el), color = "grey", fill_opacity = 0.7))
+
+fgp = folium.FeatureGroup(name="Populations")
+
+fgp.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read()), 
+style_function=lambda x: {"fillColor" : "green" if x ["properties"] ["POP2005"] < 1000000 
+else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+
+map.save("Map1.html")
